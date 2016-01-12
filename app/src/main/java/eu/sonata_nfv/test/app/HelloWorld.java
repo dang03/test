3cdc5e0a18a6c56ce5759d4535649f4cde549340
Michael Bredel <michael@bredel.at>
2016-01-12 15:21:53 +0100

    Slightly modified

--- app/src/main/java/eu/sonata_nfv/test/app/HelloWorld.java
+++ app/src/main/java/eu/sonata_nfv/test/app/HelloWorld.java
@@ -5,6 +5,6 @@ package eu.sonata_nfv.test.app;
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello SONATA World!");
   }
 }
